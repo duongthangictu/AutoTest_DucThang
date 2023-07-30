0752fa25bc6231bb54f4948a3b7c9b8d1418d60e
duongthangictu <54371511+duongthangictu@users.noreply.github.com>
2023-07-30 17:24:29 +0700

    Add files via upload

---
+++ src/main/java/flight_RoundTrip/FlightSearchResult.java
@@ -0,0 +1,8 @@
+package flight_RoundTrip;
+
+import org.openqa.selenium.By;
+
+public class FlightSearchResult {
+//	WebDriver driver;
+	public static By seachSuccesBy = By.xpath("//h2[text()='Flight Search Result']");
+}
