bda6ae3f5a80cbcebfdbb095cbd42cf629440e25
duongthangictu <54371511+duongthangictu@users.noreply.github.com>
2023-07-30 17:25:46 +0700

    Update FlightTest.java

--- src/main/java/flight_RoundTrip/FlightTest.java
+++ src/main/java/flight_RoundTrip/FlightTest.java
@@ -1,11 +1,12 @@
 package flight_RoundTrip;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
+import static org.testng.Assert.assertTrue;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import automation.common.CommonBase;
@@ -13,29 +14,23 @@ import automation.common.CommonBase;
 public class FlightTest extends CommonBase {
 	WebDriver driver;
 
-	@FindBy(xpath = "//a[@id='round-trip-tab']/parent::li")
-	private WebElement rdoRoundTripElement;
-
-	public FlightTest(WebDriver driver) {
-		this.driver = driver;
-		PageFactory.initElements(driver, this);
+	@BeforeTest
+	public void openChrome() {
+		driver = initChromeDriver("https://techydevs.com/demos/themes/html/trizen-demo/trizen/index.html");
 	}
 
 	@Test
-	public void isCheckboxSelected() {
-		driver = initChromeDriver("https://techydevs.com/demos/themes/html/trizen-demo/trizen/index.html");
-//		WebElement rdoRoundTripElement = driver.findElement(By.xpath("//a[@id='round-trip-tab']/parent::li"));
+	public void flight() {
+		FlightPage flightPage = new FlightPage(driver);
+		flightPage.fillDataSearch("Pari", "Chiness");
 
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].click()", rdoRoundTripElement);
-
-		boolean isSelected = rdoRoundTripElement.isSelected();
-		if (isSelected == false) {
-			rdoRoundTripElement.click();
-			System.out.println("Checkbox đang được select");
-		} else {
-			System.out.println("Checkbox da được select");
-		}
+//		WebElement searchResult = driver.findElement(FlightSearchResult.seachSuccesBy);
+//		Assert.assertTrue(searchResult.isDisplayed());
+		
 	}
 
+	@AfterTest
+	public void closeChrome() {
+//		quitDriver(driver);
+	}
 }
